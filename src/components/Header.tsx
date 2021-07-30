57e6a56ef9b10c30beac80e3e29c4231c792dae6
Edgar Goetzendorff <hello@dated.fun>
2021-07-31 01:19:44 +0200

    refactor: use Card component in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 
+const { Components } = globalThis.payvo;
+const { Card } = Components;
+
 import { formatCrypto } from "../utils";
 import { Avatar } from "./Avatar";
 
@@ -13,30 +16,29 @@ export const Header = ({ selectedWallet, onClickAddress }: HeaderProps) => {
 	const ticker = selectedWallet.data.COIN;
 
 	return (
-		<div className="flex bg-theme-background p-6 border-2 rounded-lg cursor-default border-theme-primary-100 dark:border-theme-secondary-800">
-			<div className="flex items-center w-1/2 space-x-4 pr-6">
-				<Avatar imageData={selectedWallet.settings.AVATAR} />
+		<Card onClick={onClickAddress}>
+			<div className="flex">
+				<div className="flex items-center w-1/2 space-x-4 pr-6">
+					<Avatar imageData={selectedWallet.settings.AVATAR} />
 
-				<div className="flex flex-col flex-1 overflow-hidden">
-					<span className="font-semibold text-theme-secondary-text text-sm">Address</span>
-					<div
-						className="flex items-center space-x-2 whitespace-nowrap no-ligatures cursor-pointer"
-						onClick={onClickAddress}
-					>
-						<span className="font-semibold text-base text-theme-text">{selectedWallet.settings.ALIAS}</span>
-						<span className="truncate text-theme-secondary-500 dark:text-theme-secondary-700 font-semibold">
-							{selectedWallet.data.ADDRESS}
-						</span>
+					<div className="flex flex-col flex-1 overflow-hidden">
+						<span className="font-semibold text-theme-secondary-text text-sm">Address</span>
+						<div className="flex items-center space-x-2 whitespace-nowrap no-ligatures cursor-pointer">
+							<span className="font-semibold text-base text-theme-text">{selectedWallet.settings.ALIAS}</span>
+							<span className="truncate text-theme-secondary-500 dark:text-theme-secondary-700 font-semibold">
+								{selectedWallet.data.ADDRESS}
+							</span>
+						</div>
 					</div>
 				</div>
-			</div>
 
-			<div className="flex flex-col w-1/2 pl-6 border-l border-theme-secondary-300 dark:border-theme-secondary-800">
-				<span className="font-semibold text-theme-secondary-text text-sm">Balance</span>
-				<span className="font-bold text-theme-text">
-					{formatCrypto({ ticker, value: Number(balance / 1e8) })}
-				</span>
+				<div className="flex flex-col w-1/2 pl-6 border-l border-theme-secondary-300 dark:border-theme-secondary-800">
+					<span className="font-semibold text-theme-secondary-text text-sm">Balance</span>
+					<span className="font-bold text-theme-text">
+						{formatCrypto({ ticker, value: Number(balance / 1e8) })}
+					</span>
+				</div>
 			</div>
-		</div>
+		</Card>
 	);
 };
